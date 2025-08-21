30d39d9eb6cb66e19000c5cf45f2e941231af4c3
karthickprabakaran <karthickprabakran06@gmail.com>
2025-08-21 10:17:28 +0530

    Changed the Vite config - included the build dir

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  root: './frontend',
+  plugins: [react()],
+  base: '/', // Important for routing
   build: {
-    outDir: '../dist',
-  },
+    outDir: 'dist',
+    assetsDir: 'assets'
+  }
 })

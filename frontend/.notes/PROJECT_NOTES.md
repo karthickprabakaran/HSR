f1af10ee80ae1b28b2d1d773bf107d8b4b921b4c
karthickprabakaran <karthickprabakaran06@gmail.com>
2025-11-13 21:11:25 +0530

    Made the Header as the Margin as the Client Asked

--- frontend/.notes/PROJECT_NOTES.md
+++ frontend/.notes/PROJECT_NOTES.md
@@ -1,7 +1,1 @@
 
--- Create an minimal Special offers page (partially done, should add the images) completed the wireframe
-
--- Create the Lawn and Events page (include the images) completed the wireframe
-
-
-
---
+++ frontend/src/components/pages/test.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Test  = () => {
+  return (
+    <div>
+      <h1>this is the test Component</h1>
+      <h2>Here Comese the First Heading</h2>
+    </div>
+  )
+}
+
+export default Test; 
